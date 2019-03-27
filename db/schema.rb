c8ff7dd54bd04c5580b9b537fff5505b4ef18b2a
Anastasia Malissovas <anastasiamalissovas@gmail.com>
2019-03-27 19:59:53 -0400

    created 2nd model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_27_235151) do
+ActiveRecord::Schema.define(version: 2019_03_27_235815) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.integer "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_characters_on_game_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "name"
